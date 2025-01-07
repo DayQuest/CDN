2263284b4cf4efc45b3a6b5d4310baf6ddd60e60
Sebastian-Stepper <sebastian.stepper@mr-pixel.de>
2025-01-08 00:40:27 +0100

    Update worker.go

--- internal/storage/worker.go
+++ internal/storage/worker.go
@@ -222,10 +222,6 @@ func (vp *VideoProcessor) createThumbnail(videoPath string) (string, error) {
     cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
     err := cmd.Run()
 
-    if err != nil {
-        fmt.Printf("Warning: failed to extract thumbnail for %s, error: %v\n", videoPath, err)
-        return vp.getFallbackThumbnailPath(), nil
-    }
 
     return thumbnailPath, nil
 }
